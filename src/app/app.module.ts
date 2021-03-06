e2b96fc4f1c3df9c7cbe34dcbefa7ceb7c41ca9e
DawidR <DawidJavaScript@gmail.com>
2021-03-06 17:43:07 +0100

    szkielet projektu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MealComponent } from './meal/meal.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MealComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/meal/meal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meal',
+  templateUrl: './meal.component.html',
+  styleUrls: ['./meal.component.less']
+})
+export class MealComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
