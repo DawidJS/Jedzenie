6433c8b7831fa28980b4f285ec1a96e178e3727e
DawidR <DawidJavaScript@gmail.com>
2021-03-10 20:34:50 +0100

    Revert "Merge branch 'master' into Dinner"
    This reverts commit eb65ec41e7a33db3530885bb7a2ab1e258229593, reversing
    changes made to 22397eefcb927fee2c462b5246ae1b9252793ac9.

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,53 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { Product } from '../models/Product';
-import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.less'],
+  styleUrls: ['./products.component.less']
 })
 export class ProductsComponent implements OnInit {
-  productsList: Product[] = [];
-  newProduct: Product = {
-    name: '',
-    kcal: 0,
-  };
 
-  constructor(private dataServices: DataService) {}
+  constructor() { }
 
-  showList() {
-    this.productsList = this.dataServices.getProducts();
-    console.log(this.productsList);
+  ngOnInit(): void {
   }
 
-
-
-
-  addList() {
-    this.productsList.push(this.newProduct);
-    this.dataServices.setProducts(this.productsList);
-    this.newProduct = {
-      name: '',
-      kcal: 0
-    }
-  }
-
-  removeList(product: Product){
-    this.productsList = this.productsList.filter(e => e!==product);
-    this.dataServices.setProducts(this.productsList);
-  }
-
-  editList(product: Product){
-    this.productsList = this.productsList.filter(e => e!==product);
-    this.dataServices.setProducts(this.productsList);
-
-    this.productsList.push(product);
-    this.dataServices.setProducts(this.productsList);
-    console.log(this.productsList);
-
-
-  }
-
-  ngOnInit(): void {}
 }
